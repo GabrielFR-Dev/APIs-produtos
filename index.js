ba116c5dfbbf85c340bd6298e01f1d0fb1df9797
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-07-04 15:28:25 -0300

    Verificando se um produto existe no banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { CadastraProdutos } from './servico/cadastraProdutos.js';
+import { CadastraProdutos, ProdutoExiste } from './servico/cadastraProdutos.js';
 import { validaProdutos } from './validacao/validacao.js'
 import { RetornaProdutos, RetornaProdutosId } from './servico/retornaProdutos.js';
 
@@ -10,36 +10,41 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/produtos', async(req, res) => {
+app.get('/produtos', async (req, res) => {
     const categoria = req.query.categoria
     const produtos = await RetornaProdutos(categoria);
     res.status(200).json(produtos);
 
 })
 
-app.get('/produtos/:id', async(req, res) => {
+app.get('/produtos/:id', async (req, res) => {
     const id = parseInt(req.params.id)
     const usuariosId = await RetornaProdutosId(id);
 
-    if(usuariosId.length > 0 ){
+    if (usuariosId.length > 0) {
         res.status(200).json(usuariosId)
     }
     else {
-        res.status(404).send({mensagem: "Produto não encontrado"})
+        res.status(404).send({ mensagem: "Produto não encontrado" })
     }
 
 })
 
 
-app.post('/cadastrar', async(req, res) => {
+app.post('/cadastrar', async (req, res) => {
     const nome = req.body.nome;
     const categoria = req.body.categoria;
     const quantidade = req.body.quantidade;
     const validade = req.body.validade;
 
-   const campoValido = validaProdutos(nome, categoria, quantidade, validade)
+    const campoValido = validaProdutos(nome, categoria, quantidade, validade)
+    const produtoExiste = await ProdutoExiste(nome)
 
-     if (campoValido.status) {
+    if(produtoExiste) {
+        return res.status(409).json({mensagem: "Já existe um produto com esse nome."})
+    }
+
+    if (campoValido.status) {
         await CadastraProdutos(nome, categoria, quantidade, validade);
         res.status(201).send({ mensagem: "Usuário cadastrado com sucesso" });
     }
@@ -54,7 +59,7 @@ app.post('/cadastrar', async(req, res) => {
 
 
 
-app.listen('3001', async() => {
+app.listen('3001', async () => {
     const data = new Date();
     console.log(`Servidor Iniciado em ${data}`);
 })
